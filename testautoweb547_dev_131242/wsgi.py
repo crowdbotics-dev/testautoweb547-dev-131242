fc7e4d3ef3a2759af746e1ad06098f70a3e63bc5
Crowdbotics <engineering@crowdbotics.com>
2023-12-02 00:09:26 +0000

    Initial commit

---
+++ testautoweb547_dev_131242/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for testautoweb547_dev_131242 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "testautoweb547_dev_131242.settings"
+)
+
+application = get_wsgi_application()
